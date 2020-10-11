71c6e67db7b4fdb02bde003599a7ef82a34c4e96
Anne Vee <vujoanne@gmail.com>
2020-10-11 18:12:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -35,5 +35,10 @@ function ternaryTeenager(age) {
 };
 
 function switchAge(age) {
-
+  if(age >= 13 && age <= 19){
+    return "You are a teenager!";
+  }
+  else{
+    return "You have an age";
+  }
 };
